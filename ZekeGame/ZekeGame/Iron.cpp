a5b80a4463e4aaff8e4dcd9d8a9bde3decf00b38
Zeke <hnta3574@gmail.com>
2018-08-09 00:07:35 +0900

    ItemFlags

---
+++ ZekeGame/ZekeGame/Iron.cpp
@@ -0,0 +1,8 @@
+#include "stdafx.h"
+#include "Iron.h"
+
+Iron::Iron() {
+
+}
+Iron::~Iron() {
+}
--- ZekeGame/ZekeGame/ItemManager.h
+++ ZekeGame/ZekeGame/ItemManager.h
@@ -4,6 +4,11 @@ class ItemManager
 public:
 	ItemManager();
 	~ItemManager();
-
+	enum Item {
+		PROBE,
+		ROVER,
+		NUM_ITEM
+	};
+	bool ItemFlags[NUM_ITEM];
 };
 
