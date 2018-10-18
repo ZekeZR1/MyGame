63adb2ce1fb332351564996b6562b1b71d8a65bb
ZekeZR1 <hnta3574@gmail.com>
2018-10-18 12:04:20 +0900

    add Effect Engine class.

--- ZekeGame/ZekeGame/graphics/Effect/CEffect.h
+++ ZekeGame/ZekeGame/graphics/Effect/CEffect.h
@@ -1,20 +1,2 @@
 #pragma once
-class CEffect
-{
-public:
-	CEffect();
-	~CEffect();
-	void Init();
-	void Play();
-	void Stop();
-	void Update();
-	void Draw();
-private:
-	 ::Effekseer::Manager*			g_manager = NULL;
-	 ::EffekseerRenderer::Renderer*	g_renderer = NULL;
-	 ::Effekseer::Effect*				g_effect = NULL;
-	 ::Effekseer::Handle				g_handle = -1;
-	 ::Effekseer::Vector3D			g_position;
-
-};
 
